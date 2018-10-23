131d10ad5ae35e636996a768cbf504befcdcde3c
srikanth Karra <srikanthreddy376@gmail.com>
2018-10-23 12:24:09 +0200

    fixed bugs

--- myFirstScript.js
+++ myFirstScript.js
@@ -1,47 +1,77 @@
 //form validation function
+document.getElementById("myform").addEventListener('submit',validateMyform);
 
-function validateMyform() {
+function validateMyform(e) {
+  //prevents submitting the empty form
+      e.preventDefault(); 
   //get all the elements form the DOM
      var firstName = document.getElementById("fName").value,
         lastName = document.getElementById("lName").value,
         dateInput = document.getElementById("dateInput").value,
         Gender = document.querySelector('input[name="sex"]:checked').value,
-        details = document.getElementById("details"),
-        output = "";
+        showResults = document.getElementById("details");
+        output = ""; 
 
     //Check for each and every vale in the form 
 
-                if(firstName == "" && lastName == ""){
-                      alert("your first or last name is missing");
+                if(!firstName == "" ){
+
+                  output += "your name is :<strong> "+ firstName + "</strong> <br>";
+
                  }else {
-                      output += "your name is <strong> "+ firstName + " " + lastName +"</strong> <br>";
-                      details.innerHTML =output;
+
+                  alert(" your FirstName is Missing");
+                  document.getElementById("fName").focus();
+                   return false;
+               } 
+                 
+                if(!lastName == ""){
+                  output += "your name is :<strong> "+ lastName + "</strong> <br>";
+                
+                }else {
+                  alert(" your LastName is Missing"); 
+                  lastName = document.getElementById("lName").focus();
+                   return false;
                 } 
-                if(dateInput == ""){
-                      alert("you forgot to enter your birthdate"); 
-                    
+
+                
+                if(!dateInput == ""){
+
+                    output +="your birthday is on :<strong> " + dateInput +" </strong><br>" ;
+                      
                 } else {
-                     output +="your birthday is <strong> " + dateInput +" </strong><br>" ;
-                     details.innerHTML =output;
+                   
+                     alert("Your Date of Birth Field is missing"); 
+                      dateInput = document.getElementById("dateInput").focus();
+                      return false;
                 }
-                if(Gender == "") {
-                    alert("your gender details are missing");      
+                if(!Gender == "") {
+                        output += "You are a :<strong>"+Gender+"</strong>";
+                        
                 } else {
-                        output += "You are a <strong>"+Gender+"</strong>";
-                        details.innerHTML =output;
+                       
+                        alert("your gender details are missing"); 
+                        document.querySelectorAll('input[name="sex"]').focus();
+                        return false; 
                 }
+              
+              showResults.innerHTML = output; 
+              document.getElementById("myform").reset();
+              return true;
+               
 }
 //Toggle button when you select the Gender 
 
-function toggleFM(gender) {
-  if( gender == "male") {
-    document.querySelector(".sex ").style.borderColor = "red";
-    document.getElementById("heading").textContent = "Hello Sir !";
-  } else {
-    document.querySelector(".sex ").style.borderColor = "blue";
-    document.getElementById("heading").textContent = "Hello Madam !";
- }
-}
+          function toggleFM(gender) {
+            if( gender == "male") {
+              document.querySelector(".sex ").style.borderColor = "red";
+              document.getElementById("heading").textContent = "Hello Sir !";
+            } else {
+              document.querySelector(".sex ").style.borderColor = "blue";
+              document.getElementById("heading").textContent = "Hello Madam !";
+          }
+      }
+
 
 
    
